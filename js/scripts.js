81f34c56cc886047d31d47ba22f4c3f695dd4582
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-03-06 15:10:08 +0300

    add a business logic function for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Business Logic
+  function Pizza(flavor, size, crust, toppings) {
+    this.pizzaFlavor = flavor;
+    this.pizzaSize = size;
+    this.pizzaCrust = crust;
+    this.pizzaToppings = toppings;
+  }
+});
