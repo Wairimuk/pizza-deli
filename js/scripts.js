7ccff8ed015198e6f474cf652892537c7ecb89fc
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-03-07 21:55:09 +0300

    add a prototype for the first constructor

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,8 @@ $(document).ready(function () {
     this.pizzaCrust = crust;
     this.pizzaToppings = toppings;
   }
-  
+  Pizza.prototype.pizzaOrder = function () {
+    return this.Flavour + this.pizzaSize + this.pizzaCrust + this.pizzaToppings;
+  };
+  // UI Logic
 });
